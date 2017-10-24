7b2eae08885f0eae44f234d24283b814b01aa6c7
yuriy_beloff <paganismrus@gmail.com>
2017-10-24 10:36:17 +0300

    исправлены замечания

--- pprint_json.py
+++ pprint_json.py
@@ -9,8 +9,8 @@ def load_data(filepath):
 
 
 def pretty_print_json(data_for_pprint):
-    prettyjson = json.dumps(json.loads(data_for_pprint), indent=4, sort_keys=True, ensure_ascii=False)
-    return prettyjson
+    pretty_json = json.dumps(json.loads(data_for_pprint), indent=4, sort_keys=True, ensure_ascii=False)
+    return pretty_json
 
 
 if __name__ == '__main__':
